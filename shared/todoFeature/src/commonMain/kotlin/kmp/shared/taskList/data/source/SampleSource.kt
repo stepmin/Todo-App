044db2bd3df90ddb31b4123653be4ef0e35306db
stepan.minarik <rohliq@gmail.com>
2025-02-08 18:49:45 +0100

    Refactoring

--- shared/todoFeature/src/commonMain/kotlin/kmp/shared/taskList/data/source/SampleSource.kt
+++
@@ -1,9 +0,0 @@
-package kmp.shared.taskList.data.source
-
-import kmp.shared.base.Result
-import kmp.shared.taskList.domain.model.Task
-import kotlinx.coroutines.flow.Flow
-
-internal interface TaskSource {
-    fun observeTasks(): Flow<Result<List<Task>>>
-}
