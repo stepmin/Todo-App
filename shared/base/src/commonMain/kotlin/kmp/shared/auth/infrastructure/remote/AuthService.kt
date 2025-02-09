e6181230a1cd4db87bae1fe862b7b39b3d64ca7a
stepan.minarik <rohliq@gmail.com>
2025-02-09 09:44:54 +0100

    Typo in package name

--- shared/base/src/commonMain/kotlin/kmp/shared/auth/infrastructure/remote/AuthService.kt
+++ shared/base/src/commonMain/kotlin/kmp/shared/auth/infrastructure/remote/AuthService.kt
@@ -2,7 +2,7 @@ package kmp.shared.auth.infrastructure.remote
 
 import io.ktor.client.HttpClient
 import kmp.shared.base.Result
-import kmp.shared.base.infrastucture.remote.clearBearerTokens
+import kmp.shared.base.infrastructure.remote.clearBearerTokens
 import kmp.shared.base.util.extension.success
 
 internal class AuthService(private val client: HttpClient) {
